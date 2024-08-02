bc171313ad1998e49fdbdcc2c30e96e2a82619de
minhttran <trcongminh.261002@gmail.com>
2024-08-02 16:38:31 +0700

    Use redux persist to rehydrate only certain values

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,12 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import { useSelector } from 'react-redux'
 
 const App = () => {
+  const {user} = useSelector((state) => state.user);
+  console.log(user)
+
   return (
     <div className="dark">
       <Router>
