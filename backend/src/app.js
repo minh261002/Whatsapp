bfda538944142a743398377b0e190cb63ab3d9ab
minhttran <trcongminh.261002@gmail.com>
2024-08-02 15:38:31 +0700

    Custom logger & handling errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,50 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import fileUpload from 'express-fileupload';
+import cors from 'cors';
+import createHttpError from 'http-errors';
 
 const app = express();
+dotenv.config();
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(mongoSanitize());
+app.use(cookieParser());
+app.use(compression());
+app.use(fileUpload({
+    useTempFiles: true,
+}));
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
 
 app.get('/', (req, res) => {
-    res.send('API is running...');
+    res.send('Hello World');
 });
+
+app.use(async(req, res, next) => {
+    next(createHttpError.NotFound('This route does not exist'));
+});
+
+app.use(async(err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message || 'Internal Server Error',
+        }
+    })
+})
 
 export default app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,34 @@
 import app from './app.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import logger from './configs/logger.config.js';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+let server;
+server = app.listen(PORT, () => {
+    console.log(process.pid);
+    logger.info(`Server is running on port ${PORT}`);
+});
+
+const exitHandler = () => {
+    if(server){
+        logger.info('Server is closing...');
+        process.exit(1);
+    }else{
+        process.exit(1);
+    }
+}
+
+const unexpectedErrorHandler = (error) => {
+    logger.error(error);
+    exitHandler();
+}
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+    if(server){
+        logger.info('SIGTERM received');
+        process.exit(1);
+    }
 });
